37612f9a063db50f7a37dc8f7b973efa6adce618
Timurdjumaniyazov <dtstimdjumaniyazov@gmail.com>
2026-08-21 00:22:25 +0500

    add new test cases

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -6,5 +6,11 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
-  // test('')
+  test('ensure navigation from the main page to the page of a particular Pokemon works', async ({ page }) => {
+    await page.goto('')
+    await page.waitForSelector('a:has(div:has-text("ivysaur"))')
+    await page.click('a:has(div:has-text("ivysaur"))')
+    await expect(page.getByText('overgrow')).toBeVisible()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
 })
